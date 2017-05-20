1f309ee4d032752f7fdd80386e2a2f4e79ff03d7
Dimitris <grmitsos@gmail.com>
2017-05-20 18:29:21 +0300

    Inital commit

---
+++ src/AppBundle/DataProvider/TouristAttractionCollectionDataProvider.php
@@ -0,0 +1,116 @@
+<?php
+
+// src/AppBundle/DataProvider/TouristAttractionCollectionDataProvider.php
+
+namespace AppBundle\DataProvider;
+
+use AppBundle\Entity\GeoCoordinates;
+use AppBundle\Entity\TouristAttraction;
+use AppBundle\Entity\PostalAddress;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Request;
+use ApiPlatform\Core\DataProvider\CollectionDataProviderInterface;
+use ApiPlatform\Core\Exception\ResourceClassNotSupportedException;
+
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Common\Persistence\ObjectManager;
+use Unirest;
+
+final class TouristAttractionCollectionDataProvider implements CollectionDataProviderInterface
+{
+  protected $requestStack;
+  protected $managerRegistry;
+  // protected $objectManager;
+
+  public function __construct(RequestStack $requestStack,ManagerRegistry $managerRegistry)
+    {
+        $this->requestStack = $requestStack;
+        $this->managerRegistry = $managerRegistry;
+        // $this->objectManager = $objectManager;
+    }
+
+    public function getCollection(string $resourceClass, string $operationName = null)
+    {
+        if (TouristAttraction::class !== $resourceClass) {
+            throw new ResourceClassNotSupportedException();
+        }
+
+        // Retrieve Search parameters
+        $request = $this->requestStack->getCurrentRequest();
+        $searchParametersObj = $request->query->all();
+        $searchParametersKeys = array_keys($searchParametersObj);
+        // dump($searchParametersObj);
+        // dump($searchParametersKeys);
+        $searchQuery = [];
+        $variable = '';
+        foreach ($searchParametersObj as $key => $value) {
+            if(is_array($value)){
+                // foreach ($value as $i => $arrayValue) {
+                //     $chainPropsKey = explode("_", $key);
+                //     $propertyKey = end($chainPropsKey);
+                //     dump($propertyKey);
+                //     $searchQuery[$propertyKey] = $value;
+                //     dump($searchQuery[$propertyKey]);
+                // }
+            } else {
+                $chainPropsKey = explode("_", $key);
+                $propertyKey = end($chainPropsKey);
+                // dump($propertyKey);
+                $searchQuery[$propertyKey] = $value;
+                // dump($searchQuery[$propertyKey]);
+            }
+        }
+
+        // dump($searchQuery);
+        $url = 'https://api.sandbox.amadeus.com/v1.2/points-of-interest/yapq-search-circle';
+        // $url = 'https://api.sandbox.amadeus.com/v1.2/points-of-interest/yapq-search-text';
+        $headers = array('Accept' => 'application/json');
+        $query = array();
+        //IF we have City we search by city
+        if(array_key_exists('addressLocality',$searchQuery)) {
+          $query['city_name'] = $searchQuery['addressLocality'];
+          $url = 'https://api.sandbox.amadeus.com/v1.2/points-of-interest/yapq-search-text';
+        } else {
+          // $query['city_name'] = 'Athens';
+          $query['radius'] = 50;
+          if(array_key_exists('latitude',$searchQuery)) {
+            $query['latitude'] = $searchQuery['latitude'];
+          } else {
+            $query['latitude'] = 37.9703;
+          }
+          if(array_key_exists('longitude',$searchQuery)) {
+            $query['longitude'] = $searchQuery['longitude'];
+          } else {
+            $query['longitude'] = 23.7278;
+          }
+        }
+        //40.640063, 22.944419 Thessaloniki
+        // $quert['category'] = 'Museum';//Landmark,Church
+        $query['apikey'] = 'ZRjgUbT6jlJZlEvY86DrhyOrXAGzvANA';
+        // $query['city_name'] = 'Athens';
+        $query['number_of_images'] = 1;
+        $query['number_of_results'] = 20;
+        // $query['latitude'] = 37.9703;
+        // $query['longitude'] = 23.7278;
+        $response = Unirest\Request::get($url,$headers,$query);
+        // dump($response);
+        $data = $response->body->points_of_interest;
+        $attr = array();
+        foreach ($data as $key => $value) {
+          $attr[$key] = new TouristAttraction();
+          $attr[$key]->setId($key);
+          $attr[$key]->setName($value->title);
+          $attr[$key]->setDescription($value->details->short_description);
+          $attr[$key]->setUrl($value->details->wiki_page_link);
+          $attr[$key]->setHasMap($value->location->google_maps_link);
+          $geo = new GeoCoordinates();
+          $geo->setId($key);
+          $geo->setLatitude($value->location->latitude);
+          $geo->setLongitude($value->location->longitude);
+          $attr[$key]->setGeo($geo);
+          $attr[$key]->setImage($value->main_image);
+       }
+        return [$attr];
+    }
+}
---
+++ src/AppBundle/Entity/TouristAttraction.php
@@ -0,0 +1,271 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * A tourist attraction.
+ *
+ * @see http://schema.org/TouristAttraction Documentation on Schema.org
+ *
+ * @ORM\Entity
+ * @ApiResource(type="http://schema.org/TouristAttraction",
+ *              iri="http://schema.org/TouristAttraction",
+ *              attributes={"filters"={"attraction.search"},
+ *                     "normalization_context"={"groups"={"readAttraction"}},
+ *                     "denormalization_context"={"groups"={"writeAttraction"}}
+ *             },
+ *             collectionOperations={
+ *                 "get"={"method"="GET", "hydra_context"={"@type"="schema:searchAction",
+ *                                                         "target"="/tourist_attractions",
+ *                                                         "query"={"@type"="vocab:#GeoCoordinates"},
+ *                                                         "result"="vocab:#TouristAttraction",
+ *                                                         "object"="vocab:#TouristAttraction"
+ *                                         }},
+ *                 "post"={"method"="POST"}
+ *             }
+ *             )
+ */
+class TouristAttraction
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string The name of the item
+     *
+     * @ORM\Column(nullable=true)
+     * @Assert\Type(type="string")
+     * @ApiProperty(iri="http://schema.org/name")
+     * @Groups({"readAttraction", "writeAttraction"})
+     */
+    private $name;
+
+    /**
+     * @var string A description of the item
+     *
+     * @ORM\Column(nullable=true)
+     * @Assert\Type(type="string")
+     * @ApiProperty(iri="http://schema.org/description")
+     * @Groups({"readAttraction", "writeAttraction"})
+     */
+    private $description;
+
+    /**
+     * @var string URL of the item
+     *
+     * @ORM\Column(nullable=true)
+     * @Assert\Url
+     * @ApiProperty(iri="http://schema.org/url")
+     * @Groups({"readAttraction", "writeAttraction"})
+     */
+    private $url;
+
+    /**
+     * @var string A URL to a map of the place
+     *
+     * @ORM\Column(nullable=true)
+     * @Assert\Url
+     * @ApiProperty(iri="http://schema.org/hasMap")
+     * @Groups({"readAttraction", "writeAttraction"})
+     */
+    private $hasMap;
+
+    /**
+     * @var GeoCoordinates The geo coordinates of the place
+     *
+     * @ORM\OneToOne(targetEntity="AppBundle\Entity\GeoCoordinates")
+     * @ApiProperty(iri="http://schema.org/geo")
+     * @Groups({"readAttraction", "writeAttraction"})
+     */
+    private $geo;
+
+    /**
+     * @var string An image of the item. This can be a \[\[URL\]\] or a fully described \[\[ImageObject\]\]
+     *
+     * @ORM\Column(nullable=true)
+     * @Assert\Url
+     * @ApiProperty(iri="http://schema.org/image")
+     * @Groups({"readAttraction", "writeAttraction"})
+     */
+    private $image;
+
+    /**
+     * Sets id.
+     *
+     * @param int $id
+     *
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Sets name.
+     *
+     * @param string $name
+     *
+     * @return $this
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Gets name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Sets description.
+     *
+     * @param string $description
+     *
+     * @return $this
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Gets description.
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Sets url.
+     *
+     * @param string $url
+     *
+     * @return $this
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Gets url.
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Sets hasMap.
+     *
+     * @param string $hasMap
+     *
+     * @return $this
+     */
+    public function setHasMap($hasMap)
+    {
+        $this->hasMap = $hasMap;
+
+        return $this;
+    }
+
+    /**
+     * Gets hasMap.
+     *
+     * @return string
+     */
+    public function getHasMap()
+    {
+        return $this->hasMap;
+    }
+
+    /**
+     * Sets geo.
+     *
+     * @param GeoCoordinates $geo
+     *
+     * @return $this
+     */
+    public function setGeo(GeoCoordinates $geo = null)
+    {
+        $this->geo = $geo;
+
+        return $this;
+    }
+
+    /**
+     * Gets geo.
+     *
+     * @return GeoCoordinates
+     */
+    public function getGeo()
+    {
+        return $this->geo;
+    }
+
+    /**
+     * Sets image.
+     *
+     * @param string $image
+     *
+     * @return $this
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Gets image.
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+}
